d143babf979295a97cbcadfc9114e5acb1a6dafe
Bartłomiej Foks <uiq3.eu@gmail.com>
2018-02-26 19:49:57 +0100

    disable force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment() !== 'local') {
-            URL::forceScheme('https');
-        }
+//        if ($this->app->environment() !== 'local') {
+//            URL::forceScheme('https');
+//        }
     }
 
     /**
