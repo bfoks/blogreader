33c05f0ce21de3ad45c87776dc88196e73f2b586
Bartłomiej Foks <uiq3.eu@gmail.com>
2018-02-27 01:25:48 +0100

    remove the unused external stylesheet

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,10 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="styles.css">
     <title>blogreader.pro beta</title>
-
-    {{--<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">--}}
 
     <style>
         html, body {
