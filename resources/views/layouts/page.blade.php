240acc321a8a5c23816613964e5c9adc589a4359
Bartłomiej Foks <uiq3.eu@gmail.com>
2018-03-01 01:36:47 +0100

    UI

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -5,7 +5,7 @@
     <div id="wrapper" class="flex flex-col flex-no-wrap h-full">
 
         {{-- Navbar --}}
-        <div class="z-20 flex flex-no-shrink justify-between items-center bg-white px-4 py-2 border-b-2 border-orange">
+        <div class="z-20 flex flex-no-shrink justify-between items-center px-4 py-2 border-b-2 border-orange">
             <div><a class="no-underline  text-sm sm:text-2xl" href="{{ route('index') }}">
                     @include('_partials.logo')
                 </a></div>
