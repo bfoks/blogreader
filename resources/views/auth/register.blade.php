1d78d42e69acc2d0f606b86af7c74eee82067258
Bartłomiej Foks <uiq3.eu@gmail.com>
2018-03-01 18:10:54 +0100

    UI registration autocomplete

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <div class="flex items-center justify-center h-full container mx-auto">
 
-        <form class="w-full sm:w-1/2 lg:1/3 flex items-center justify-center flex-col border-t-4 border-orange rounded shadow mx-1" method="POST" action="{{ route('register') }}">
+        <form autocomplete="on" class="w-full sm:w-1/2 lg:1/3 flex items-center justify-center flex-col border-t-4 border-orange rounded shadow mx-1" method="POST" action="{{ route('register') }}">
 
             <div class="flex w-full items-center justify-center flex-col border-r border-b border-l border border-grey-light bg-white p-6">
 
                 @csrf
 
-                <input id="email" type="email" class="my-2 p-2 border w-full" name="email" value="{{ old('email') }}" placeholder="Email" required>
+                <input autocomplete="email" id="email" type="email" class="my-2 p-2 border w-full" name="email" value="{{ old('email') }}" placeholder="Email" required>
 
                 @if ($errors->has('email'))
                     <span class="text-red text-sm">
@@ -17,7 +17,7 @@
                 </span>
                 @endif
 
-                <input id="password" type="password" class="my-2 p-2 border w-full" name="password" placeholder="Hasło" required>
+                <input autocomplete="new-password" id="password" type="password" class="my-2 p-2 border w-full" name="password" placeholder="Hasło" required>
 
                 @if ($errors->has('password'))
                     <span class="text-red text-sm">
@@ -25,7 +25,7 @@
                 </span>
                 @endif
 
-                <input id="password-confirm" type="password" class="my-2 p-2 border w-full" name="password_confirmation" placeholder="Powtórz hasło"
+                <input autocomplete="new-password" id="password-confirm" type="password" class="my-2 p-2 border w-full" name="password_confirmation" placeholder="Powtórz hasło"
                        required>
 
                 <button type="submit" class="btn mt-2">
